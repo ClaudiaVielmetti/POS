24f445024ba0ec6a9619898b1b7d8da24bd15a38
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-04-07 15:01:08 +0200

    Update addItem.php

--- 02-pos-system.zip.02-pos-system/02-pos-system/include/addItem.php
+++ 02-pos-system.zip.02-pos-system/02-pos-system/include/addItem.php
@@ -1,8 +1,12 @@
 <?php
 
-function addItem(MenuItem $menuItem) {
 
-    // business logic here..
-
-    return;
+class MenuItem {
+    public $name;
+    public $price;
+    public function __construct($name, $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
 }
+
