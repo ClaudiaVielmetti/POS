7652817170cbf80eb728a4ac2a259c9803843e4f
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-04-07 15:04:18 +0200

    Update addItem.php

--- 02-pos-system.zip.02-pos-system/02-pos-system/include/addItem.php
+++ 02-pos-system.zip.02-pos-system/02-pos-system/include/addItem.php
@@ -1,12 +1,21 @@
 <?php
 
 
-class MenuItem {
+class MenuItem
+{
     public $name;
     public $price;
-    public function __construct($name, $price) {
+    public function __construct($name, $price)
+    {
         $this->name = $name;
         $this->price = $price;
     }
 }
 
+function addItem(MenuItem $menuItem)
+{
+    // Add the menu item to the menu
+    $menu[] = $menuItem;
+    // Do any additional business logic here...
+    return;
+}
