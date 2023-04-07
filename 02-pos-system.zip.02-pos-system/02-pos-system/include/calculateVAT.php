e80a15019451def4971ac5244024e864c2f289d0
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-04-07 15:02:32 +0200

    Update calculateVAT.php

--- 02-pos-system.zip.02-pos-system/02-pos-system/include/calculateVAT.php
+++ 02-pos-system.zip.02-pos-system/02-pos-system/include/calculateVAT.php
@@ -1,8 +1,10 @@
 <?php
 
-function calculateVAT($PurchasedItemsTotal) {
-
-    // business logic here...
-
-    return;
+function calculateVAT($purchasedItemsTotal, $vatRate = 20) {
+    // Calculate the VAT amount
+    $vatAmount = $purchasedItemsTotal * ($vatRate / 100);
+    // Round the VAT amount to 2 decimal places
+    $vatAmount = round($vatAmount, 2);
+    // Return the VAT amount
+    return $vatAmount;
 }
